cb9fb99e196d01cc91909aff721c43f5a57b27fe
wanghm16 <2509553015@qq.com>
2024-04-28 12:23:03 +0800

    2024.4.28

--- leetcode/t17.cpp
+++ leetcode/t17.cpp
@@ -5,28 +5,50 @@ using namespace std;
 
 class Solution
 {
-public:
-    vector<int> maxSlidingWindow(vector<int> &nums, int k)
+    vector<vector<int>> edge;
+    vector<int> vis;
+    bool valid = true;
+
+    void dfs(int u)
     {
-        int n = nums.size(), j = 0;
-        deque<int> dq;
-        vector<int> res(n - k + 1);
-        for (int i = 0; i < n; i++)
+        vis[u] = 1;
+        for (int v : edge[u])
         {
-            while (!dq.empty() && i - k + 1 > dq.front())
+            if (!vis[v])
             {
-                dq.pop_front();
+                dfs(v);
+                if (!valid)
+                    return;
             }
-            while (!dq.empty() && nums[i] >= nums[dq.back()])
+            else if (vis[v] == 1)
             {
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            if (i >= k - 1)
-            {
-                res[j++] = nums[dq.front()];
+                valid = false;
+                return;
             }
         }
-        return res;
+        vis[u] = 2;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        edge.resize(numCourses);
+        vis.resize(numCourses);
+        for (auto &e : prerequisites)
+        {
+            edge[e[1]].push_back(e[0]);
+        }
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (!vis[i])
+            {
+                dfs(i);
+                if (!valid)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
