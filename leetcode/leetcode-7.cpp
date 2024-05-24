6214c03aa07516cd63b80d2fae4e0003e730f0d4
昊旻 王 <211250107@smail.nju.edu.cn>
2024-05-24 17:52:10 +0800

    fix: 修改lc-7解法，更好记忆理解

--- leetcode/leetcode-7.cpp
+++ leetcode/leetcode-7.cpp
@@ -7,23 +7,17 @@ class Solution
 public:
     int reverse(int x)
     {
-        if (x == 0)
-            return 0;
-        int res = 0;
+        int rev = 0;
         while (x != 0)
         {
-            int tmp = x % 10;
-            if (res > 214748364 || (res == 214748364 && tmp > 7))
+            if (rev < INT_MIN / 10 || rev > INT_MAX / 10)
             {
                 return 0;
             }
-            if (res < -214748364 || (res == -214748364 && tmp < -8))
-            {
-                return 0;
-            }
-            res = res * 10 + tmp;
+            int digit = x % 10;
             x /= 10;
+            rev = rev * 10 + digit;
         }
-        return res;
+        return rev;
     }
 };
