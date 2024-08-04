b3ab6145faf76c6a09a0abb92cb4ded9b4283b29
Haomin Wang <211250107@smail.nju.edu.cn>
2024-08-04 11:23:13 +0800

    2024.8.4

--- leetcode/temp.cpp
+++ leetcode/temp.cpp
@@ -6,37 +6,28 @@ using namespace std;
 class Solution
 {
 public:
-    int strStr(string haystack, string needle)
+    vector<int> rightSideView(TreeNode *root)
     {
-        int n = haystack.size(), m = needle.size();
-        vector<int> ne(m);
-        for (int i = 1, j = 0; i < m; i++)
+        vector<int> res;
+        if (!root)
+            return res;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
         {
-            while (j > 0 && needle[i] != needle[j])
+            int n = q.size();
+            for (int i = 0; i < n; i++)
             {
-                j = ne[j - 1];
-            }
-            if (needle[i] == needle[j])
-            {
-                j++;
-            }
-            ne[i] = j;
-        }
-        for (int i = 0, j = 0; i < n; i++)
-        {
-            while (j > 0 && haystack[i] != needle[j])
-            {
-                j = ne[j - 1];
-            }
-            if (haystack[i] == needle[j])
-            {
-                j++;
-            }
-            if (j == m)
-            {
-                return i - m + 1;
+                TreeNode *p = q.front();
+                q.pop();
+                if (i == n - 1)
+                    res.push_back(p->val);
+                if (p->left)
+                    q.push(p->left);
+                if (p->right)
+                    q.push(p->right);
             }
         }
-        return -1;
+        return res;
     }
 };
